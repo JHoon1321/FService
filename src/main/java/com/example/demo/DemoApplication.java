9fce602c32bbe888337ffbf886306154e6de5486
JHoon1321 <94960858+JHoon1321@users.noreply.github.com>
2022-03-12 15:19:57 +0900

    부트스트랩 적용

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,8 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		String str = "abcd";
+		System.out.println(str);
 	}
 
 }
